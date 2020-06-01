8c4f74bdefdca57ea42ae827e6ad6c4883d4d1a7
Tynx <sweetloveinyourheart@gmail.com>
2020-06-01 17:26:29 +0700

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//Connect to MongoDB
+
+
+//MiddleWare
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+//router
+
+
+const PORT = process.env.PORT || 9999;
+const sever = http.createServer(app);
+
+sever.listen(PORT, () => {
+    console.log(`Sever is running at http://localhost/${PORT}`)
+})
